d90747b7808da8592d9ecb66cf7d3d3b9f664038
Eric@IHT <ee.ren@eevolve.club>
2018-01-26 16:23:47 +0800

    Updated contracts to eth capped

--- contracts/MintedEthCappedCrowdsale.sol
+++ contracts/MintedEthCappedCrowdsale.sol
@@ -4,30 +4,33 @@ import "./Crowdsale.sol";
 import "./MintableToken.sol";
 
 /**
- * ICO crowdsale contract that is capped by amout of tokens.
+ * ICO crowdsale contract that is capped by amout of ETH.
  *
  * - Tokens are dynamically created during the crowdsale
  *
  *
  */
-contract MintedTokenCappedCrowdsale is Crowdsale {
+contract MintedEthCappedCrowdsale is Crowdsale {
 
-  /* Maximum amount of tokens this crowdsale can sell. */
-  uint256 public maximumSellableTokens;
+  /* Maximum amount of wei this crowdsale can raise. */
+  uint public weiCap;
 
-  function MintedTokenCappedCrowdsale(address _token, PricingStrategy _pricingStrategy, address _multisigWallet, uint256 _start, uint256 _end, uint256 _minimumFundingGoal, uint256 _maximumSellableTokens, address _tokenVestingAddress) Crowdsale(_token, _pricingStrategy, _multisigWallet, _start, _end, _minimumFundingGoal, _tokenVestingAddress) public {
-    maximumSellableTokens = _maximumSellableTokens;
-  }
+  function MintedEthCappedCrowdsale(address _token, PricingStrategy _pricingStrategy, 
+    address _multisigWallet, uint256 _start, uint256 _end, uint256 _minimumFundingGoal, uint256 _weiCap, address _tokenVestingAddress) 
+    Crowdsale(_token, _pricingStrategy, _multisigWallet, _start, _end, _minimumFundingGoal,_tokenVestingAddress) public
+    { 
+      weiCap = _weiCap;
+    }
 
   /**
    * Called from invest() to confirm if the curret investment does not break our cap rule.
    */
   function isBreakingCap(uint256 weiAmount, uint256 tokenAmount, uint256 weiRaisedTotal, uint256 tokensSoldTotal) public constant returns (bool limitBroken) {
-    return tokensSoldTotal > maximumSellableTokens;
+    return weiRaisedTotal > weiCap;
   }
 
   function isCrowdsaleFull() public constant returns (bool) {
-    return tokensSold >= maximumSellableTokens;
+    return weiRaised >= weiCap;
   }
 
   /**
